c0d8dcc40de0c40d8d2c62e7696cc3b1793e3ecd
egarza27 <elizabethigarza10@gmail.com>
2023-02-20 20:52:47 -0600

    added css styling

--- main.js
+++ main.js
@@ -1,17 +1,33 @@
-let wordList = ["cat", "dog", "hippo", "horse", "turtle", "shark"];
+let wordList = [
+  "cheese",
+  "pepporoni",
+  "pineapple",
+  "sausage",
+  "onions",
+  "jalapeno",
+];
 
+// calling wordChosen function to randomly choose word from word list array
 let word = wordChosen(wordList);
-
 console.log(word);
 
+// making an array of letters
 let wordArr = word.split("");
 
+// chaning letters of wordArr to dashes using map method
 let hiddenWord = wordArr.map((x) => "-");
 console.log(hiddenWord);
 
-const getInput = (input) => (userInput = input);
+let result1 = [];
 
-const resultField = document.getElementById("result");
+// captures value of input value field in DOM
+const inputValue = document.getElementById("userInput");
+
+const goButton = document.querySelector("#goButton");
+
+const result = document.querySelector("#result");
+
+// ? ------------------- function section below ----------------------
 
 function wordChosen(wordList) {
   let index = Math.floor(Math.random() * wordList.length);
@@ -19,33 +35,51 @@ function wordChosen(wordList) {
   return word;
 }
 
-const filterLetter = (userInput, word) => {
-  for (let i = 0; i < word.length; i++) {
-    if (word[i] === userInput) {
-      hiddenWord = word[i];
-    }
-  }
-  return hiddenWord.push("");
+// TODO: check if inputValue is in the wordArr
+
+const checkValue = (array) => {
+  array.reduce((a, e, i) => {
+    if (e === inputValue.value) a.push(i);
+    result1 = a;
+    return result1;
+  }, []);
 };
 
-console.log(filterLetter("o", hiddenWord));
+// go button has an onclick event listener and upon the event, checkValue function will run
+goButton.addEventListener("click", (e) => {
+  checkValue(wordArr);
 
-// function randomWord() {
-//   let randomIndex = Math.floor(Math.random() * wordList.length);
-//   // let randomWord = arr[randomIndex];
-//   return randomWord;
-// }
+  if (result1.length === 0 && !checkValue(wordArr)) {
+    window.alert(`Incorrect guess!`);
+  } else {
+    replaceDash(wordArr, result1);
+  }
+});
 
-// function hideWord() {
-//   for (let i = 0; i < wrd.length; i++) {
-//     wordShown.push("-");
-//   }
-//   return wordShown;
-// }
+// TODO: if value is true, replace dash with inputValue. if false, mark as incorrect guess
 
-// const hideWord = (wrd) => {
-//   for (let i = 0; i < wrd.length; i++) {
-//     wordShown.push("-");
-//   }
-//   return wordShown;
-// };
+const replaceDash = (wordArray, indexArray) => {
+  checkForWin();
+
+  indexArray.forEach((index) => {
+    hiddenWord[index] = inputValue.value;
+    console.log(hiddenWord);
+  });
+  result.innerHTML = hiddenWord;
+};
+
+// TODO: keep track of player's incorrect and correct guesses
+
+// TODO: determine winner or loser
+
+const checkForWin = () => {
+  console.log(wordArr);
+
+  console.log(hiddenWord);
+
+  if (wordArr === hiddenWord) {
+    window.alert(`You won!`);
+  } else return false;
+};
+
+// console.log(checkForWin());
